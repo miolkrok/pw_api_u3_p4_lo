c26cab94c70fc82b1f9bc5c9afe7529c61afa67f
luis <luis@gmail.com>
2023-07-22 10:51:31 -0500

    Taller 32

--- src/main/java/com/example/pw_api_u3_p4_lo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/pw_api_u3_p4_lo/controller/EstudianteControllerRestFul.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pw_api_u3_p4_lo.repository.modelo.Estudiante;
@@ -28,16 +30,30 @@ public class EstudianteControllerRestFul {
     private IEstudianteService estudianteService;
 
     // GET
-    @GetMapping(path = "/{cedula}")
-    public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula) {
-        // return this.estudianteService.consultarPorCedula(cedula);
-        return ResponseEntity.status(227).body(this.estudianteService.consultarPorCedula(cedula));
+    @GetMapping(path = "/{cedula}", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Estudiante consultarPorCedula(@PathVariable String cedula) {
+
+        return this.estudianteService.consultarPorCedula(cedula);
+        // return
+        // ResponseEntity.status(227).body(this.estudianteService.consultarPorCedula(cedula));
     }
-        @GetMapping(path = "/status/{cedula}")
-    public ResponseEntity<Estudiante> consultarPorCedulaStatus(@PathVariable String cedula) {
-        // return this.estudianteService.consultarPorCedula(cedula);
-        return ResponseEntity.status(HttpStatus.OK).body(this.estudianteService.consultarPorCedula(cedula));
-    }
+
+    // @GetMapping(path = "/{cedula}")
+    // public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String
+    // cedula) {
+    // // return this.estudianteService.consultarPorCedula(cedula);
+    // return
+    // ResponseEntity.status(227).body(this.estudianteService.consultarPorCedula(cedula));
+    // }
+
+    // @GetMapping(path = "/status/{cedula}")
+    // public ResponseEntity<Estudiante> consultarPorCedulaStatus(@PathVariable
+    // String cedula) {
+    // // return this.estudianteService.consultarPorCedula(cedula);
+    // return
+    // ResponseEntity.status(HttpStatus.OK).body(this.estudianteService.consultarPorCedula(cedula));
+    // }
 
     // @GetMapping()
     // public List<Estudiante> consultarTodos() {
@@ -48,7 +64,7 @@ public class EstudianteControllerRestFul {
     @GetMapping
     public ResponseEntity<List<Estudiante>> consultarTodosProv(@RequestParam String provincia) {
 
-        //return this.estudianteService.buscarTodosProv(provincia);
+        // return this.estudianteService.buscarTodosProv(provincia);
         List<Estudiante> lista = this.estudianteService.buscarTodosProv(provincia);
         HttpHeaders cabeceras = new HttpHeaders();
         cabeceras.add("detalleMensaje", "Ciudadanos consultados exitosamente");
@@ -56,9 +72,17 @@ public class EstudianteControllerRestFul {
         return new ResponseEntity<>(lista, cabeceras, 228);
     }
 
-    @PostMapping
-    public void guardar(@RequestBody Estudiante estudiante) {
+    // @PostMapping(consumes = "application/xml")
+    // public void guardar(@RequestBody Estudiante estudiante) {
+    // this.estudianteService.guardar(estudiante);
+    // }
+
+    @PostMapping(path = "/{cedula}", produces = "application/xml", consumes = "application/xml")
+    public Estudiante guardar(@RequestBody Estudiante estudiante,@PathVariable String cedula) {
+        estudiante.setCedula(cedula);
         this.estudianteService.guardar(estudiante);
+        return this.estudianteService.consultarPorCedula(estudiante.getCedula());
+         
     }
 
     @PutMapping(path = "/{identificador}")
